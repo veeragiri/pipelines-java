b37429dffb533d75249efc2b5c3a2ca68cf0e747
Veera Mangipudi <veera.mangipudi@gmail.com>
2026-08-09 12:46:59 +0300

    Create NegativeTest.java
    adding negative test

---
+++ src/test/java/NegativeTest.java
@@ -0,0 +1,14 @@
+import com.microsoft.demo.Demo;
+import org.junit.Test;
+
+public class NegativeTest {
+    @Test
+    public void test_method_1() {
+        Demo d = new Demo();
+        d.DoSomething(false);
+    }
+
+    @Test
+    public void test_method_2() {
+    }
+}
